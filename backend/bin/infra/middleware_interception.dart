23cb6c604b768780698454c3115a7d5a7cb6957c
pedroabel <pedrooabell@gmail.com>
2023-06-01 03:26:06 -0300

    01.06 - Cors

--- backend/bin/infra/middleware_interception.dart
+++ backend/bin/infra/middleware_interception.dart
@@ -1,11 +1,28 @@
 import 'package:shelf/shelf.dart';
 
-class MiddlewareInterception {
-  Middleware get middleware => createMiddleware(
+class MInterception {
+  static Middleware get contentTypeJson => createMiddleware(
         responseHandler: (Response res) => res.change(
           headers: {
             'content-type': 'application/json',
           },
         ),
       );
+
+  static Middleware get cors {
+    final headersAllowed = {'Access-Control-Allow-Origin': '*'};
+
+    Response? handlerOption(Request req) {
+      if (req.method == 'OPTIONS') {
+        return Response(200, headers: headersAllowed);
+      } else {
+        return null;
+      }
+    }
+
+    Response addCorsHeader(Response res) => res.change(headers: headersAllowed);
+
+    return createMiddleware(
+        requestHandler: handlerOption, responseHandler: addCorsHeader);
+  }
 }
