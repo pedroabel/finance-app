9474f000ad53c96cb0dad932a5d582b7afbd0974
voidPedro <pedrooabell@gmail.com>
2023-06-03 00:37:54 -0300

    03.06 - Login Screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,23 +1,72 @@
+import 'package:finance/welcome_screen.dart';
 import 'package:flutter/material.dart';
 
-import '/screens/welcome_page.dart';
+import 'constants.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      title: 'Finapp',
       theme: ThemeData(
-        fontFamily: 'Poppins',
-        scaffoldBackgroundColor: Colors.blueGrey[50],
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.blue,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            minimumSize: const Size(double.infinity, 48),
+            backgroundColor: const Color(0xFFF2994A),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(14),
+              side: BorderSide.none,
+            ),
+            elevation: 0,
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          fillColor: const Color(0xFFFBFBFB),
+          filled: true,
+          border: defaultOutlineInputBorder,
+          enabledBorder: defaultOutlineInputBorder,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(14),
+            borderSide: const BorderSide(color: Color(0xFFF2994A)),
+          ),
+        ),
       ),
-      home: const WelcomePage(),
+      home: const WelcomeScreen(),
     );
   }
 }
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// import 'trash/screens/welcome_page.dart';
+
+// void main() {
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       theme: ThemeData(
+//         fontFamily: 'Poppins',
+//         scaffoldBackgroundColor: Colors.blueGrey[50],
+//       ),
+//       home: const WelcomePage(),
+//     );
+//   }
+// }
---
+++ frontend/lib/welcome_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../auth/login_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/welcome_bg.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Color(0xFF111122)],
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SvgPicture.asset('assets/logo/Logo_white.svg'),
+                  const Spacer(flex: 14),
+                  const Text(
+                    'Gerencie seu Dinheiro de Forma Descomplicada',
+                    style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const Spacer(flex: 1),
+                  Text(
+                    'Organize, planeje e conquiste sua liberdade financeira com facilidade',
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                      );
+                    },
+                    child: const Text("Avançar"),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
